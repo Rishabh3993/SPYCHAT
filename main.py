9e6be65963d55cf431dd6c62c9bd5bdbc302a302
Rishabh3993 <rishabhsingla1994@gmail.com>
2017-06-21 19:27:37 +0530

    Selecting user from menu created and dictionaries made

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# we import some variable from spy_details.py
-from spy_details import spy_name, spy_age, spy_salutation, spy_rating, spy_online
+# we import some variable/dictionary from spy_details.py
+from spy_details import spy
 
 # implementing list
 status_message_list=['My name is Mr. ABCD', 'Shaken, not stirred', 'Enf of life']
 
-# implementing empty list
-friend_name = []
-friend_age = []
-friend_rating = []
-friend_online = []
+# implementing empty list and use of dictionary
+friends =[]
 
 print'WELCOME SPY!'
 # backslash is used  for ignoring that next character
 print 'Let\'s get started with your profile'
 
-# making of option to the user
-question_continue_or_not='Do you want to continue as '+spy_salutation+''+spy_name+'(Y/N)'
+'''
+triple quotes can be used for multiple line commenting
+making of option yes or no to the user
+spy['name']-use to read the value from dictionary
+'''
+question_continue_or_not='Do you want to continue as '+spy['salutation']+''+spy['name']+'(Y/N)'
 existing=raw_input(question_continue_or_not)
 
 # creating function for status adding , variables: add_status(to add new), current_status( for showing current)
@@ -60,46 +61,56 @@ def add_status(current_status):
     return updated_status
 
 def add_friend():
-    new_name=raw_input('Please enter others Spy friend name :')
-    new_salutation=raw_input("Please enter salutation of your spy friend :")
-    new_age=int(raw_input('Enter age of spy friend :'))
-    new_rating=float(raw_input('Enter the rating :'))
+    new_friend={
+        'name':'',
+        'salutation':'',
+        'age':'0',
+        'rating':'0.0'
+    }
+
+    new_friend['name']=raw_input('Please enter others Spy friend name :')
+    new_friend['salutation']=raw_input("Please enter salutation of your spy friend :")
+    new_friend['age']=int(raw_input('Enter age of spy friend and age should be greater then 12 and less then 50 :'))
+    new_friend['rating']=float(raw_input('Enter the rating :'))
 
 
-    if len(new_name)>0 and new_age>12 and new_age<50 and new_rating>=spy_rating:
-        friend_name.append(new_name)
-        friend_age.append(new_age)
-        friend_rating.append(new_rating)
-        friend_online.append(True)
+    if len(new_friend['name'])>0 and new_friend['age']>12 and new_friend['age']<50 and new_friend['rating']>=spy['rating']:
+        friends.append(new_friend)
         print('Spy Friend Added!')
 
     else:
         print('Sorry!Please try to add a valid spy with proper details')
 
-    return len(friend_name)
+    return len(friends)
 
+def select_friend():
+    item_number=0
+    for friend in friends:
+        print '%d.%s aged %d with rating of %.2f is online'%(item_number+1,friend['name'],friend['age'],friend['rating'])
+        item_number=item_number+1
 
-
-
+    friend_selection=raw_input("Choose a friend \n >")
+    friend_selection_position=int(friend_selection)-1
+    return friend_selection_position
 
 
 # creating function
-def start_chat(spy_name,spy_age,spy_rating):
+def start_chat(spy):
     current_status_message = None
 
 
 
-    if spy_age >12 and spy_age <50:
+    if spy['age'] >12 and spy['age'] <50:
 
         # we use Placeholder here %s for string , %d for integer and %f for float
-        print'Process completed.Welcome' +spy_name+' Age: %d and Rating of:%.2f.Welcome to spyChat' % (spy_age, spy_rating)
+        print'Process completed.Welcome' +spy['name']+' Age: %d and Rating of:%.2f.Welcome to spyChat' % (spy['age'], spy['rating'])
 
         show_menu = True
 
         while show_menu:
             menu_choices='Please select the option \n 1. Add a status to update \n 2. Add a friend \n ' \
-                        '3. Send Secret Message \n 4.Read a secret Message \n 5. Read chat from user \n ' \
-                        '6. Close Application \n >'
+                        '3. Select spy friend \n 4. Send Secret Message \n 5.Read a secret Message \n ' \
+                         '6. Read chat from user \n 7. Close Application \n'
             menu_choice=raw_input(menu_choices)
 
             if len(menu_choice)>0:
@@ -110,6 +121,9 @@ def start_chat(spy_name,spy_age,spy_rating):
                 elif menu_choice==2:
                     number_of_friend_list=add_friend()
                     print 'You have %d friends'%(number_of_friend_list)
+                elif menu_choice==3:
+                    index=select_friend()
+                    print index
 
                 else:
                     show_menu=False
@@ -117,52 +131,60 @@ def start_chat(spy_name,spy_age,spy_rating):
         print 'Your age is not valid to be a spy'
 
 if existing=='Y':
-    start_chat(spy_name,spy_age,spy_rating)
+    start_chat(spy)
 
 else:
+    spy = {
+        'name': '',
+        'salutation': '',
+        'age': 0,
+        'rating': 0.0,
+        'is_online': False
+    }
 
     #spy_name is variable and raw_input is method to input the value in variable which is given by user
-    spy_name=raw_input('Welcome to spy chat, tell me your spy name first:')
+    spy['name']=raw_input('Welcome to spy chat, tell me your spy name first:')
 
-    if len(spy_name)>0:
+    if len(spy['name'])>0:
 
 
-        spy_salutation=raw_input('Should i call you Mr. or Ms. ?')
+        spy['salutation']=raw_input('Should i call you Mr. or Ms. ?')
 
         # when we again declare the same variable it overload that value and show to us
         # when we use space between single quote like ' ' then space is also count as string
-        spy_name=spy_salutation+' '+spy_name
+        spy['name']=spy['salutation']+' '+spy['name']
         #  +  is used for joining(string concatenation) to string
-        print'Welome ' + spy_name + '.Glad to see you again!'
+        print'Welome ' + spy['name'] + '.Glad to see you again!'
+
 
 
         # now we add more information by making some variables
         # we use int(raw_input()) to convert the string into integer directly
-        spy_age=int(raw_input('Enter your age:'))
+        spy['age']=int(raw_input('Enter your age:'))
 
         # we can use and / == to compare two things
-        if spy_age>12 and spy_age<50:
-            spy_rating=float(raw_input('Enter your spy rating:'))
+        if spy['age']>12 and spy['age']<50:
+            spy['rating']=float(raw_input('Enter your spy rating:'))
 
             # for cases we use if elif(use in making of many cases) and finally else
-            if (spy_rating>4.5):
+            if (spy['rating']>4.5):
                 print'You are awesome spy'
-            elif (spy_rating>3.5 and spy_rating<=4.5):
+            elif (spy['rating']>3.5 and spy['rating']<=4.5):
                 print 'You are good spy '
-            elif (spy_rating>=2.5 and spy_rating<=3.5):
+            elif (spy['rating']>=2.5 and spy['rating']<=3.5):
                 print 'You can do better'
             else:
                 print'We can use somebody to help in office'
 
             # Making spy online after getting details
-            spy_online=True
+            spy['online']=True
 
 
 
         else:
             print'Sorry for inconvenience , but your age is not valid'
 
-        start_chat(spy_name, spy_age, spy_rating)
+        start_chat(spy)
 
     else:
         print'Please enter your name and try again.'
--- spy_details.py
+++ spy_details.py
@@ -1,5 +1,10 @@
-spy_name = 'ABCD'
-spy_salutation = 'Mr.'
-spy_age = 14
-spy_rating = 2.5
-spy_online = True
+
+# Making of dictionary name spy, keyword are variable and ABCD,Mr,and other are values
+spy={
+    'name':'ABCD',
+    'salutation':"Mr.",
+    'age':14,
+    'rating':2.5,
+    'online':'True'
+}
+
